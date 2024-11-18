72580808057de396d794890d03f67cff401b149c
kasra-l83 <o.kasralatifi@gmail.com>
2024-11-18 18:27:00 +0330

    Introduction part

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
   ],
   theme: {
     colors: {
-      purple: "#5D50C6"
+      purple: "#5D50C6",
+      gray: "rgb(249 250 251)",
+      pink: "#F85E9F",
+      yellow: "#FACD49",
+      orange: "#FF5722"
     },
     extend: {}
   },
