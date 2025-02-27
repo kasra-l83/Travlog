6f067fad27c7a9292dcdf8815717c9e204a78ac3
kasra-l83 <o.kasralatifi@gmail.com>
2025-02-27 18:23:23 +0330

    Header

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    colors: {
+      purple: "#5D50C6",
+      gray: "rgb(249 250 251)",
+      pink: "#F85E9F",
+      yellow: "#FACD49",
+      orange: "#FF5722"
+    },
+    extend: {}
+  },
+  plugins: []
+}
